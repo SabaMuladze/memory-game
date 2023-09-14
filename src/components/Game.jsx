5cce8ef30fcf6e11c78261d3c66fa042f12c618d
SabaMuladze <muladze66@bk.ru>
2023-09-14 20:14:38 +0400

    feat:change buttons background with click

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -10,12 +10,12 @@ const Game = () => {
     }
     return (
         <>
-            <header className="flex justify-between pt-7 px-6 items-center">
+            <header className="flex justify-between pt-7 px-6 items-center md:px-10 lg:px-[170px] lg:pt-16">
                 <h2 className="">memory</h2>
                 <button onClick={menuHandler} className="px-5 py-2 bg-[#FDA214] rounded-3xl md:hidden ">Menu</button>
                 <div className="max-md:hidden flex gap-4">
                     <button onClick={(e) => location.reload()} className="px-5 py-2 bg-[#FDA214] rounded-3xl ">Restart</button>
-                    <button className="px-5 py-2 bg-[#DFE7EC] rounded-3xl text-[#304859] ">New Game</button>
+                    <button className="px-5 py-2 bg-[#DFE7EC] rounded-3xl text-[#304859] "><Link to={'/setup'}>New Game</Link></button>
                 </div>
             </header>
             {showMenu == true ? <Menu setShowMenu={setShowMenu} /> : null}
--- src/components/GameSet.jsx
+++ src/components/GameSet.jsx
@@ -1,8 +1,13 @@
 import { Link } from "react-router-dom"
 import Game from './Game'
+import { useState } from "react"
 
 
 const GameSet = () => {
+    const [theme, setTheme] = useState('Numbers')
+    const [playerNum, setPlayerNum] = useState(1)
+    const [gridSize, setGridSize] = useState('4x4')
+
     return (
         <section className="bg-[#152938] w-full h-screen flex flex-col items-center px-6">
             <h1 className="mb-[60px] mt-20 md:mt-[169px]">memory</h1>
@@ -10,25 +15,25 @@ const GameSet = () => {
                 <div>
                     <p className="pb-3 text-[#7191A5]">Select Theme</p>
                     <div className="flex gap-3">
-                        <button className="py-[10px] w-[50%] bg-[#BCCED9] rounded-3xl">Numbers</button>
-                        <button className="py-[10px] w-[50%] bg-[#BCCED9] rounded-3xl">Icons</button>
+                        <button onClick={() => setTheme('Numbers')} className={`py-[10px] w-[50%]  rounded-3xl hover:bg-[#6395B8] ${theme == "Numbers" ? "active" : "bg-[#BCCED9]"}`}>Numbers</button>
+                        <button onClick={() => setTheme('Icons')} className={`py-[10px] w-[50%]  rounded-3xl hover:bg-[#6395B8] ${theme == "Icons" ? "active" : "bg-[#BCCED9]"}`}>Icons</button>
                     </div>
                 </div>
                 <div>
                     <p className="pb-3 text-[#7191A5]">Numbers of Players</p>
                     <div className="flex gap-3 justify-center">
-                        <button className="py-[10px] w-[25%] bg-[#BCCED9] rounded-3xl">1</button>
-                        <button className="py-[10px] w-[25%] bg-[#BCCED9] rounded-3xl">2</button>
-                        <button className="py-[10px] w-[25%] bg-[#BCCED9] rounded-3xl">3</button>
-                        <button className="py-[10px] w-[25%] bg-[#BCCED9] rounded-3xl">4</button>
+                        <button onClick={() => setPlayerNum(1)} className={`py-[10px] w-[25%] bg-[#BCCED9] rounded-3xl hover:bg-[#6395B8] ${playerNum == 1 ? "active" : "bg-[#BCCED9]"}`}>1</button>
+                        <button onClick={() => setPlayerNum(2)} className={`py-[10px] w-[25%] bg-[#BCCED9] rounded-3xl hover:bg-[#6395B8] ${playerNum == 2 ? "active" : "bg-[#BCCED9]"}`}>2</button>
+                        <button onClick={() => setPlayerNum(3)} className={`py-[10px] w-[25%] bg-[#BCCED9] rounded-3xl hover:bg-[#6395B8] ${playerNum == 3 ? "active" : "bg-[#BCCED9]"}`}>3</button>
+                        <button onClick={() => setPlayerNum(4)} className={`py-[10px] w-[25%] bg-[#BCCED9] rounded-3xl hover:bg-[#6395B8] ${playerNum == 4 ? "active" : "bg-[#BCCED9]"}`}>4</button>
                     </div>
 
                 </div>
                 <div>
                     <p className="pb-3 text-[#7191A5]">Grid Size</p>
                     <div className="flex gap-3">
-                        <button className="py-[10px] w-[50%] bg-[#BCCED9] rounded-3xl">4x4</button>
-                        <button className="py-[10px] w-[50%] bg-[#BCCED9] rounded-3xl">6x6</button>
+                        <button onClick={() => setGridSize('4x4')} className={`py-[10px] w-[50%] bg-[#BCCED9] rounded-3xl hover:bg-[#6395B8] ${gridSize == '4x4' ? "active" : "bg-[#BCCED9]"} `}>4x4</button>
+                        <button onClick={() => setGridSize('6x6')} className={`py-[10px] w-[50%] bg-[#BCCED9] rounded-3xl hover:bg-[#6395B8] ${gridSize == '6x6' ? "active" : "bg-[#BCCED9]"}`}>6x6</button>
                     </div>
                     <button className="w-full py-3 bg-[#FDA214] rounded-3xl mt-8"><Link to={'/game'} >Start Game</Link></button>
                 </div>
@@ -38,3 +43,5 @@ const GameSet = () => {
 }
 
 export default GameSet
+
+
